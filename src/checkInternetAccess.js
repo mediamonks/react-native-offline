682ee5f8a4eba850af73d612880463136d5877e4
Mient-jan Stelling <mientjan.stelling@gmail.com>
2017-08-17 16:51:13 -0300

    adding a 2 second delay before checking the internet connection through a call to google.

--- src/checkInternetAccess.js
+++ src/checkInternetAccess.js
@@ -2,26 +2,31 @@
 
 export default function checkInternetAccess(
   isConnected: boolean,
-  timeout: number = 3000,
+  timeout: number = 4000,
   address: string = 'https://google.com',
 ): Promise<boolean> {
   if (!isConnected) {
     return Promise.resolve(false);
   }
 
+  const delayWhenChecking = 2000;
+
   return new Promise((resolve: (value: boolean) => void) => {
     const tm = setTimeout(() => {
       resolve(false);
-    }, timeout);
+    }, timeout + delayWhenChecking);
 
-    fetch(address, { method: 'HEAD' })
-      .then(() => {
-        clearTimeout(tm);
-        resolve(true);
-      })
-      .catch(() => {
-        clearTimeout(tm);
-        resolve(false);
-      });
+    setTimeout(() => {
+      fetch(address, { method: 'HEAD' })
+        .then(() => {
+          clearTimeout(tm);
+          resolve(true);
+        })
+        .catch(() => {
+          clearTimeout(tm);
+          resolve(false);
+        });
+
+    }, delayWhenChecking);
   });
 }
